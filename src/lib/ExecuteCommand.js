288c42340c622297fbb4541f35d682c8790aa136
Jacky <jacky@Jackyde-MacBook-Pro.local>
2018-10-25 10:55:09 +0800

    modify ExecuteCommand function for return command details

--- src/lib/ExecuteCommand.js
+++ src/lib/ExecuteCommand.js
@@ -2,11 +2,11 @@ const cmd = require('node-cmd');
 
 module.exports = function(command) {
   return new Promise(function(resolve, reject) {
-    cmd.get(command, function (error) {
+    cmd.get(command, function (error, data) {
       if(error) {
         reject(error);
       } else {
-        resolve(true);
+        resolve(data);
       }
     });
   });
