d46d7e3f735340de89adcc0ed1271e17e7d6b473
billPortalNetwork <bill@portal.network>
2018-12-04 16:41:16 +0800

    Support NPM auto publish in CI

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
-git checkout master	
-echo "Version update" 
+git checkout release.sh
+echo "Version update"
 npm version patch
 echo "Commit"
 git commit --amend -m  "[ci skip] version changed"
 git remote -v
 echo "Push"
-git push https://${GIT_ACCOUNT}:${GIT_PW}@github.com/PortalNetwork/kaizen-cli.git master
+git push https://${GIT_ACCOUNT}:${GIT_PW}@github.com/PortalNetwork/kaizen-cli.git dev
