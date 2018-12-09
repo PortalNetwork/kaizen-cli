c73300439be052c8c5bbaf139a019360625267fb
Phyrex Tsai <x01ep23i@hotmail.com>
2018-12-09 23:19:20 +0800

    Remove test file

--- src/lib/apis.test.js
+++
@@ -1,9 +0,0 @@
-const api = require('kaizen-cli/src/lib/apis');
-
-describe('api test', async () => {
-  it('Get instance list', async () => {
-    let idToken = "";
-    const instanceList = await api.apiKaizenInstanceList(idToken);
-    console.log(instanceList);
-  });
-});
