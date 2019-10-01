ab198477950388a32af2466cbe89956b98ba2e6f
Phyrex Tsai <x01ep23i@hotmail.com>
2019-10-01 16:16:05 +0800

    Add kaizen contracts list

---
+++ src/components/Contracts/List/index.js
@@ -0,0 +1,22 @@
+const Log = require('../../../lib/Log');
+require('colors');
+
+function builder(yargs) {
+  
+}
+
+async function handler(argv) {
+  Log.NormalLog('Available contract list:');
+  Log.NormalLog('ChainLink'.underline.yellow + ' - Chainlink Oracle Service, link: ' + 'https://github.com/PortalNetwork/kaizen-contracts/tree/master/Chainlink'.underline.yellow);
+  Log.NormalLog('ERC20'.underline.yellow + ' - ERC20 Token Standard, link: ' + 'https://github.com/PortalNetwork/kaizen-contracts/tree/master/ERC20'.underline.yellow);
+  Log.NormalLog('ERC721'.underline.yellow + ' - ERC721 Token Standard, link: ' + 'https://github.com/PortalNetwork/kaizen-contracts/tree/master/ERC721'.underline.yellow);
+  Log.NormalLog('NuCypher'.underline.yellow + ' - NuCypher Contracts, link: ' + 'https://github.com/PortalNetwork/kaizen-contracts/tree/master/NuCypher'.underline.yellow)
+
+  Log.NormalLog("\nTo install a plugin run " + "'kaizen contracts deploy <contract-link-here>'".yellow)
+}
+
+module.exports = function (yargs) {
+  const command = 'list';
+  const commandDescription = 'Lists all available contracts';
+  yargs.command(command, commandDescription, builder, handler);
+}
